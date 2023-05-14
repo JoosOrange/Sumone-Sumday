f82743bf723fd6fb3282493ccfb1eb9d4abc957b
joowhan <joy980721@gmail.com>
2023-05-15 00:47:22 +0900

    sumday project 230515

--- lib/main.dart
+++ lib/main.dart
@@ -84,7 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Sumday"),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Sumday+Sumone new project:',
             ),
             Text(
               '$_counter',
